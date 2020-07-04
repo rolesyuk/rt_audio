fc05b0b3509880a1a0551ee958634c76f743b164
Roman Olesyuk <roman.olesyuk@gmail.com>
2020-07-04 11:56:32 +0300

    Synched to WINE NSPA 5.9-13 version

--- wine/build.sh
+++ wine/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# this script build DEBs for WINE NSPA https://github.com/nine7nine/pkgbuilds_nspa
+# this script builds DEBs for WINE NSPA https://github.com/nine7nine/pkgbuilds_nspa
 
 # this script does NOT handle WINE build dependencies for amd64 (WINE64) and i386 (WINE32)
 # install them as in https://wiki.winehq.org/Building_Wine#Satisfying_Build_Dependencies
@@ -20,11 +20,11 @@ function print_interactive {
 
 PKG_NAME=wine-nspa
 PKG_VER=5.9
-PKG_REL=12
+PKG_REL=13
 
 WINE_SOURCE="https://dl.winehq.org/wine/source/5.x/wine-${PKG_VER}.tar.xz"
 WINE_STAGING_SOURCE="https://github.com/wine-staging/wine-staging/archive/v${PKG_VER}.tar.gz"
-NSPA_SOURCE="https://github.com/nine7nine/pkgbuilds_nspa/archive/4b29025f3b337c328373f2d4deb5d15397b9f26e.zip"
+NSPA_SOURCE="https://github.com/nine7nine/pkgbuilds_nspa/archive/b6668f13c07d22b768697cce1edee9baba0f25a3.zip"
 
 WORKING_DIR="${PWD}"
 SCRIPT_DIR="$(dirname `readlink -f "${0}"`)"
@@ -33,6 +33,7 @@ WINE_STAGING_DIR="${WORKING_DIR}/wine-staging-${PKG_VER}"
 NSPA_DIR="${WORKING_DIR}/pkgbuilds_nspa-$(basename ${NSPA_SOURCE} | awk -F '.' '{print $1}')"
 
 # fixes BIAS FX 2 GUI not-showing isssue
+# for details see https://linuxmusicians.com/viewtopic.php?t=20660&p=111944
 PATCHES=("${SCRIPT_DIR}/cef.patch")
 
 if [ ! -r $(basename "${WINE_SOURCE}") ]; then
@@ -65,8 +66,10 @@ rm -rf ${PKG_NAME}-{32,64}-build
 mkdir ${PKG_NAME}-{32,64}-build
 
 # apply nspa patchset
-grep 'patch -s' ${NSPA_DIR}/wine-nspa/PKGBUILD | sed 's/srcdir/{NSPA_DIR}\/wine-nspa/' | while read PATCH; do
-	echo "Applying $(eval echo ${PATCH}) ..."
+N=1
+grep -E '^ *patch' ${NSPA_DIR}/wine-nspa/PKGBUILD | sed 's/srcdir/{NSPA_DIR}\/wine-nspa/' | while read PATCH; do
+	echo "$(printf '%02d:' ${N}) $(eval echo ${PATCH})"
+	N=$((N+1))
 	eval "${PATCH}"
 done
 
