5ae882aac278970516d9d9fc6647d86507560a6c
Roman Olesyuk <roman.olesyuk@gmail.com>
2020-10-17 09:02:48 +0300

    Synched to kernel 5.4.71-rt40 with PREEMPT_RT and FWM

--- kernel/build-5.4.sh
+++ kernel/build-5.4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-KERNEL_VERSION="5.4.69"
-PATCH_VERSION="5.4.69-rt39"
+KERNEL_VERSION="5.4.71"
+PATCH_VERSION="5.4.70-rt40"
 
 KERNEL_URL="https://cdn.kernel.org/pub/linux/kernel/v5.x"
 PATCH_URL="https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.4"
@@ -17,19 +17,27 @@ function build_kernel {
 	tar -xf "linux-${KERNEL_VERSION}.tar"
 
 	cd "linux-${KERNEL_VERSION}"
-	if [ "${1}" == "fwm-rt" ]; then
+	if   [ "${1}" == "rt" ]; then
 		patch -p1 < ../"patch-${PATCH_VERSION}.patch" || exit -1
 		cp "${CONFIG_RT}" .config || exit -1
+		yes "" | make oldconfig
+		make -j$(nproc) deb-pkg || exit -1
+	elif [ "${1}" == "fwm-rt" ]; then
+		patch -p1 < ../"patch-${PATCH_VERSION}.patch" || exit -1
+		patch -p1 < "${SCRIPT_DIR}/futex-wait-multiple-5.5-deadlock_fix.patch" || exit -1
+		cp "${CONFIG_RT}" .config || exit -1
+		yes "" | make oldconfig
+		make EXTRAVERSION=-fwm -j$(nproc) deb-pkg || exit -1
 	elif [ "${1}" == "fwm" ]; then
+		patch -p1 < "${SCRIPT_DIR}/futex-wait-multiple-5.5-deadlock_fix.patch" || exit -1
 		cp "${CONFIG_NO_RT}" .config || exit -1
+		yes "" | make oldconfig
+		make EXTRAVERSION=-fwm -j$(nproc) deb-pkg || exit -1
 	else
 		exit -1
 	fi
-	patch -p1 < "${SCRIPT_DIR}/futex-wait-multiple-5.5-deadlock_fix.patch" || exit -1
-	yes "" | make oldconfig
-	make EXTRAVERSION=-fwm -j$(nproc) deb-pkg || exit -1
-	cd ..
 
+	cd ..
 	rm -rf "linux-${KERNEL_VERSION}"
 }
 
@@ -45,6 +53,7 @@ unxz -k "patch-${PATCH_VERSION}.patch.xz"
 gpg --verify "linux-${KERNEL_VERSION}.tar.sign" "linux-${KERNEL_VERSION}.tar" || exit -1
 gpg --verify "patch-${PATCH_VERSION}.patch.sign" "patch-${PATCH_VERSION}.patch" || exit -1
 
+build_kernel rt
 build_kernel fwm-rt
 build_kernel fwm
 
