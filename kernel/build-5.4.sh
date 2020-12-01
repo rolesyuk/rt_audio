bc60184a20e47e17ca9bfbcb4207f8567b7edc6a
Roman Olesyuk <roman.olesyuk@gmail.com>
2020-12-01 09:41:54 +0300

    Synched kernel versions

--- kernel/build-5.4.sh
+++ kernel/build-5.4.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-KERNEL_VERSION="5.4.72"
-PATCH_VERSION="5.4.70-rt40"
+KERNEL_VERSION="5.4.80"
+PATCH_VERSION="5.4.78-rt44"
 KERNEL_URL="https://cdn.kernel.org/pub/linux/kernel/v5.x"
 PATCH_URL="https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.4"
 SCRIPT_DIR="$(dirname `readlink -f $0`)"
 CONFIG_RT="${SCRIPT_DIR}/config-5.4-rt"
 CONFIG_NO_RT="${SCRIPT_DIR}/config-5.4-no-rt"
+COMPILER="/usr/bin/gcc-10"
 
 source "${SCRIPT_DIR}/build.sh"
 
 download
-build rt
-build fwm-rt
-build fwm
+build rt opt
 cleanup
--- kernel/build-5.9.sh
+++ kernel/build-5.9.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 KERNEL_VERSION="5.9.1"
-PATCH_VERSION="5.9.1-rt18"
+PATCH_VERSION="5.9.1-rt20"
 KERNEL_URL="https://cdn.kernel.org/pub/linux/kernel/v5.x"
 PATCH_URL="https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.9"
 SCRIPT_DIR="$(dirname `readlink -f $0`)"
 CONFIG_RT="${SCRIPT_DIR}/config-5.9-rt"
-CONFIG_NO_RT="${SCRIPT_DIR}/config-5.9-no-rt"
-COMPILER="/usr/bin/gcc-10" 
+COMPILER="/usr/bin/gcc-10"
 
 source "${SCRIPT_DIR}/build.sh"
 
