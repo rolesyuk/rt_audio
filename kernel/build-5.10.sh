ce296ae87976113713e78301d09abcbcd260428f
Roman Olesyuk <roman.olesyuk@gmail.com>
2021-07-13 09:50:17 +0300

    Synched to kernel 5.10.48-rt45 and 5.13.1-rt1

--- kernel/build-5.10.sh
+++ kernel/build-5.10.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-KERNEL_VERSION="5.10.36"
-PATCH_VERSION="5.10.35-rt39"
+KERNEL_VERSION="5.10.48"
+PATCH_VERSION="5.10.47-rt45"
 KERNEL_URL="https://cdn.kernel.org/pub/linux/kernel/v5.x"
 PATCH_URL="https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.10"
 SCRIPT_DIR="$(dirname `readlink -f $0`)"
 CONFIG_RT="${SCRIPT_DIR}/config-5.10-rt"
 CONFIG_NO_RT="${SCRIPT_DIR}/config-5.10-no-rt"
-COMPILER="/usr/bin/gcc-10"
+COMPILER="/usr/bin/gcc-11"
 
 source "${SCRIPT_DIR}/build.sh"
 
---
+++ kernel/build-5.13.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+KERNEL_VERSION="5.13.1"
+PATCH_VERSION="5.13-rt1"
+KERNEL_URL="https://cdn.kernel.org/pub/linux/kernel/v5.x"
+PATCH_URL="https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.13"
+SCRIPT_DIR="$(dirname `readlink -f $0`)"
+CONFIG_RT="${SCRIPT_DIR}/config-5.13-rt"
+CONFIG_NO_RT="${SCRIPT_DIR}/config-5.13-rt"
+COMPILER="/usr/bin/gcc-11"
+
+source "${SCRIPT_DIR}/build.sh"
+
+download
+build rt opt
+build opt
+cleanup
