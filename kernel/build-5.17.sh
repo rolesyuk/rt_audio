75b6f8ec7fb041e6e602153c6c38072f1972ae71
Roman Olesyuk <roman.olesyuk@gmail.com>
2022-02-02 11:59:33 +0300

    Synched to kernel 5.17-rc2-rt3

---
+++ kernel/build-5.17.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+KERNEL_VERSION="5.17-rc2"
+PATCH_VERSION="5.17-rc2-rt3"
+KERNEL_URL="https://git.kernel.org/torvalds/t"
+PATCH_URL="https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.17"
+SCRIPT_DIR="$(dirname `readlink -f $0`)"
+CONFIG_RT="${SCRIPT_DIR}/config-5.17-rt"
+CONFIG_NO_RT=""
+COMPILER="/usr/bin/gcc-11"
+
+source "${SCRIPT_DIR}/build-rc.sh"
+
+download
+build rt opt
+cleanup
