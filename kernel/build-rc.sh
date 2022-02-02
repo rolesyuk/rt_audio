76a83fe11a4ab1cb723716e8d27547b783dc22e4
Roman Olesyuk <roman.olesyuk@gmail.com>
2022-02-02 12:02:17 +0300

    Synched to kernel 5.17-rc2-rt3

---
+++ kernel/build-rc.sh
@@ -0,0 +1,14 @@
+source $(dirname `readlink -f $0`)/build.sh
+
+function download {
+	mkdir -p "${SCRIPT_DIR}/${KERNEL_VERSION}"
+	cd "${SCRIPT_DIR}/${KERNEL_VERSION}"
+	
+	wget -c "${KERNEL_URL}/linux-${KERNEL_VERSION}.tar.gz"
+	wget -c "${PATCH_URL}/patch-${PATCH_VERSION}.patch.xz"
+	wget -c "${PATCH_URL}/patch-${PATCH_VERSION}.patch.sign"
+	
+	gunzip -k "linux-${KERNEL_VERSION}.tar.gz"
+	unxz -k "patch-${PATCH_VERSION}.patch.xz"
+	gpg --verify "patch-${PATCH_VERSION}.patch.sign" "patch-${PATCH_VERSION}.patch" || exit -1
+}
