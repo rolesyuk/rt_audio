36c648c983fd94a297d09bda6157d626fdf0e407
Roman Olesyuk <roman.olesyuk@gmail.com>
2022-06-09 23:25:25 +0300

    Synched to kernel 5.18.2-rt11

--- kernel/build-5.18.sh
+++ kernel/build-5.18.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-KERNEL_VERSION="5.17.1"
-PATCH_VERSION="5.17-rt15"
+KERNEL_VERSION="5.18.2"
+PATCH_VERSION="5.18-rt11"
 KERNEL_URL="https://cdn.kernel.org/pub/linux/kernel/v5.x"
-PATCH_URL="https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.17"
+PATCH_URL="https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.18"
 SCRIPT_DIR="$(dirname `readlink -f $0`)"
-CONFIG_RT="${SCRIPT_DIR}/config-5.17-rt"
+CONFIG_RT="${SCRIPT_DIR}/config-5.18-rt"
 CONFIG_NO_RT=""
 COMPILER="/usr/bin/gcc-11"
 
