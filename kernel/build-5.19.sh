ad4b914b7306284ba01b5e5220b32567599a9bda
Roman Olesyuk <roman.olesyuk@gmail.com>
2022-08-04 22:33:23 +0300

    Synched to kernel 5.19-rt10

---
+++ kernel/build-5.19.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+KERNEL_VERSION="5.19"
+PATCH_VERSION="5.19-rt10"
+KERNEL_URL="https://cdn.kernel.org/pub/linux/kernel/v5.x"
+PATCH_URL="https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.19"
+SCRIPT_DIR="$(dirname `readlink -f $0`)"
+CONFIG_RT="${SCRIPT_DIR}/config-5.19-rt"
+CONFIG_NO_RT=""
+COMPILER="/usr/bin/gcc-11"
+
+source "${SCRIPT_DIR}/build.sh"
+
+download
+build rt opt
+cleanup
