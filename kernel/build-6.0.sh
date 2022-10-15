368d9be4cb39f8f66062ba2cf71a87ba9940d1ae
Roman Olesyuk <roman.olesyuk@gmail.com>
2022-10-15 22:50:11 +0300

    Synched to kernel 6.0.2-rt11

---
+++ kernel/build-6.0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+KERNEL_VERSION="6.0.2"
+PATCH_VERSION="6.0-rt11"
+KERNEL_URL="https://cdn.kernel.org/pub/linux/kernel/v6.x"
+PATCH_URL="https://cdn.kernel.org/pub/linux/kernel/projects/rt/6.0"
+SCRIPT_DIR="$(dirname `readlink -f $0`)"
+CONFIG_RT="${SCRIPT_DIR}/config-6.0-rt"
+CONFIG_NO_RT=""
+COMPILER="/usr/bin/gcc-11"
+
+source "${SCRIPT_DIR}/build.sh"
+
+download
+build rt opt
+cleanup
