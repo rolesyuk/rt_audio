84242bceed4bdbdccf5e9d531542053ee5bcd089
Roman Olesyuk <roman.olesyuk@gmail.com>
2022-12-20 15:33:14 +0300

    Synched to kernel 6.1-rt5

---
+++ kernel/build-6.1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+KERNEL_VERSION="6.1"
+PATCH_VERSION="6.1-rc7-rt5"
+KERNEL_URL="https://cdn.kernel.org/pub/linux/kernel/v6.x"
+PATCH_URL="https://cdn.kernel.org/pub/linux/kernel/projects/rt/6.1"
+SCRIPT_DIR="$(dirname `readlink -f $0`)"
+CONFIG_RT="${SCRIPT_DIR}/config-6.1-rt"
+CONFIG_NO_RT=""
+COMPILER="/usr/bin/gcc-11"
+
+source "${SCRIPT_DIR}/build.sh"
+
+download
+build rt opt
+cleanup
