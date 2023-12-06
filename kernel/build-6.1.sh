d0619a842c81338e115bb61ff382a3e66e81399b
Roman Olesyuk <roman.olesyuk@gmail.com>
2023-12-06 21:51:49 +0300

    Synched to kernel 6.1.65-rt18

--- kernel/build-6.1.sh
+++ kernel/build-6.1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-KERNEL_VERSION="6.1.64"
-PATCH_VERSION="6.1.64-rt17"
+KERNEL_VERSION="6.1.65"
+PATCH_VERSION="6.1.65-rt18"
 KERNEL_URL="https://cdn.kernel.org/pub/linux/kernel/v6.x"
 PATCH_URL="https://cdn.kernel.org/pub/linux/kernel/projects/rt/6.1"
 SCRIPT_DIR="$(dirname `readlink -f $0`)"
